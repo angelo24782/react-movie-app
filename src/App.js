9790b7f6b150ea8d8589b55becffdc3c14c5b15e
Angelo <angelo24782@gmail.com>
2020-07-02 19:29:34 +0200

    update style app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Movie Database</h1>
       </header>
     </div>
